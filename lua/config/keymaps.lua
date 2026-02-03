44edf979b270e2f0c786641b30596b5fd042b9e9
Will <will.mccallion@icloud.com>
2026-02-03 16:12:47 -0700

    New keymaps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 --- @module config.keymaps
---- @brief Defines global keybindings for Neovim.
---- @description This module configures custom shortcuts for navigation, line manipulation,
---- system clipboard integration, buffer management, and Lua development utilities.
+--- @brief Global keybindings and custom shortcuts configuration.
+--- @description Sets up keymaps for navigation, buffer/window management, clipboard operations,
+--- and various utility functions to enhance the Neovim experience.
 
 vim.keymap.set({ "n", "x" }, "j", "gj", { desc = "Navigate down (visual line)" })
 vim.keymap.set({ "n", "x" }, "k", "gk", { desc = "Navigate up (visual line)" })
@@ -30,3 +30,44 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 vim.keymap.set("n", "<leader>xx", "<Cmd>source %<CR>", { desc = "Source current file" })
 vim.keymap.set("n", "<leader>x", "<Cmd>:.lua<CR>", { desc = "Lua: execute current line" })
 vim.keymap.set("v", "<leader>x", "<Cmd>:lua<CR>", { desc = "Lua: execute current selection" })
+
+vim.keymap.set(
+	"n",
+	"<leader>rw",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Replace [W]ord under cursor" }
+)
+
+vim.keymap.set("x", "<leader>rw", [["hy:%s/<C-r>h/<C-r>h/gI<Left><Left><Left>]], { desc = "Replace selection" })
+
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Resize window height +" })
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Resize window height -" })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Resize window width -" })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Resize window width +" })
+
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and stay in visual" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and stay in visual" })
+vim.keymap.set("x", "p", '"_dP', { desc = "Paste without overwriting register" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result and center" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Prev search result and center" })
+
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+vim.keymap.set("n", "<leader>rf", function()
+	local current_file = vim.fn.expand("%")
+	local new_name = vim.fn.input("Rename file: ", current_file)
+	if new_name ~= "" and new_name ~= current_file then
+		vim.cmd("saveas " .. new_name)
+		vim.cmd("e " .. new_name)
+		vim.cmd("!rm " .. current_file)
+		print("Renamed to " .. new_name)
+	end
+end, { desc = "[R]ename current [F]ile" })
