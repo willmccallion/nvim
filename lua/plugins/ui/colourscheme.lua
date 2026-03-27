61597c47e52c218e8d31e9ea4a86dde3d7775aa8
Will <will.mccallion@icloud.com>
2026-03-27 13:24:24 -0600

    Added tokyo night.

--- lua/plugins/ui/colourscheme.lua
+++ lua/plugins/ui/colourscheme.lua
@@ -3,12 +3,20 @@
 --- @description This module handles the installation and configuration of the Nightfox
 --- colorscheme family, specifically applying the Terafox variant with transparency.
 
-vim.pack.add({ "https://github.com/EdenEast/nightfox.nvim" })
+vim.pack.add({ 
+  "https://github.com/EdenEast/nightfox.nvim",
+  "https://github.com/folke/tokyonight.nvim",
+})
 
 require("nightfox").setup({
 	options = {
 		transparent = true,
 	},
+})
+
+require("tokyonight").setup({
+	transparent = true,
+  style = "night",
 })
 
 vim.cmd([[colorscheme terafox]])
