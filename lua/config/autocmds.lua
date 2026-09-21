3849b8a514dfeeb336a8a797c71ece5b6bada5ad
Will McCallion <will.mccallion@icloud.com>
2026-09-21 13:53:59 -0600

    Highlight a put as well as a yank

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,8 @@
 --- Global autocommands.
---- Highlight text briefly after yanking.
+--- Highlight text briefly after yanking or putting it.
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
+vim.api.nvim_create_autocmd({ "TextYankPost", "TextPutPost" }, {
+	desc = "Highlight the text a yank or a put just covered",
 	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
 	callback = function()
 		vim.hl.hl_op()
